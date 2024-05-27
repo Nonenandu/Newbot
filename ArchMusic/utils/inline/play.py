e3e3f7d71a14ca543cf3a95ebbe543ff434838dd
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2024-05-27 15:43:38 +0530

    Update play.py

--- ArchMusic/utils/inline/play.py
+++ ArchMusic/utils/inline/play.py
@@ -2,8 +2,6 @@ import math
 
 from pyrogram.types import InlineKeyboardButton
 
-from ArchMusic.utils.formatters import time_to_seconds
-
 
 def track_markup(_, videoid, user_id, channel, fplay):
     buttons = [
